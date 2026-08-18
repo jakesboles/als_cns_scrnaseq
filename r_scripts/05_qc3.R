7c1575d883dabadde21a87669acc42941484b0ef
jakesboles <jake.boles@northwestern.edu>
2026-08-18 17:04:17 -0500

    Updated/cleaned 05 plotting script

--- r_scripts/05_qc3.R
+++ r_scripts/05_qc3.R
@@ -1,26 +1,52 @@
-library(tidyverse)
-library(ggplot2)
-library(paletteer)
-library(ggbeeswarm)
-library(scales)
-library(patchwork)
+suppressMessages({
+  library(tidyverse)
+  library(ggplot2)
+  library(paletteer)
+  library(ggbeeswarm)
+  library(scales)
+  library(patchwork)
+})
 
-proj_dir <- "/projects/b1169/boles/als_multitissue_scfrp/"
+setwd("/projects/b1169/boles/als_cns_scrnaseq")
 
-meta <- read.csv(paste0(proj_dir, "tab_data/metadata.csv"))
-
-plots_dir <- paste0(proj_dir, "plots/05_qc3/")
+plots_dir <- "plots/05_qc3/"
 dir.create(plots_dir, 
            showWarnings = F,
            recursive = T)
+
+# in_dir <- "data/04_doubletfinder/metadata_persample/"
+# 
+# files <- list.files(in_dir)
+# 
+# for (i in seq_along(files)){
+#   
+#   message(paste0(i, " out of ", length(files)))
+#   
+#   if (i == 1) { 
+#     meta <- readRDS(paste0(in_dir, files[i]))
+#     
+#     col_idx <- str_detect(colnames(meta), "pANN", negate = T)
+#     
+#     meta <- meta[, col_idx]
+#   } else { 
+#     meta2 <- readRDS(paste0(in_dir, files[i]))
+#     
+#     col_idx <- str_detect(colnames(meta2), "pANN", negate = T)
+#     
+#     meta2 <- meta2[, col_idx]
+#     
+#     meta <- rbind(meta, meta2)
+#   }
+# }
+
+meta <- read.csv("tab_data/metadata.csv")
 
 
 # Get pre-filter median stats & contamination ---------------------------------------------
 
 pre_stats_list <- list()
 
-pre_stats_files <- list.files(paste0(proj_dir, 
-                                     "tab_data/02_qc1"),
+pre_stats_files <- list.files("tab_data/02_qc1",
                               full.names = T)
 stats_tissues <- c("Motor cortex", "Muscle", "Cervical spinal cord")
 
@@ -34,7 +60,7 @@ pre_stats <- list_rbind(pre_stats_list)
 
 pre_stats <- pre_stats %>%
   dplyr::rename("ID" = "id") %>%
-  mutate(ID = str_replace_all(ID, "GWF-", "GWF")) %>%
+  # mutate(ID = str_replace_all(ID, "GWF-", "GWF")) %>%
   left_join(meta, by = c("ID", "Tissue")) %>%
   mutate(Batch = factor(Batch,
                         levels = c(1:6)),
@@ -45,16 +71,19 @@ pre_stats <- pre_stats %>%
                          levels = c("Motor cortex", "Cervical spinal cord", "Muscle"),
                          labels = c("Motor cortex", "Cervical\nspinal cord", "Skeletal muscle")))
 
-contam <- read.csv(paste0(proj_dir, 
-                          "tab_data/00_soupx/contamination_fraction_fixed.csv"))[1:90, ] %>%
-  separate(id, into = c("ID", "Tissue"),
-           sep = "_") %>%
-  mutate(Tissue = factor(Tissue, 
-                         levels = c("b", "s", "m"),
-                         labels = c("Motor cortex", "Cervical\nspinal cord", "Skeletal muscle")))
+contam <- read.csv("tab_data/00_cellbender/cellbender_metrics.csv")[1:90, ] %>%
+  dplyr::select(-c(pool, group)) %>% 
+  # separate(id, into = c("ID", "Tissue"),
+  #          sep = "_") %>%
+  mutate(tissue = factor(tissue, 
+                         levels = c("Motor cortex", "Cervical spinal cord", "Muscle"),
+                         labels = c("Motor cortex", "Cervical\nspinal cord", "Skeletal muscle"))) %>%
+    dplyr::rename("Tissue" = "tissue",
+                  "ID" = "sample")
 
 pre_stats <- pre_stats %>%
-  left_join(contam)
+  left_join(contam,
+            by = c("Tissue", "ID"))
 # Median QC of counts, features, and mito before filtering ----------------
 
 
@@ -108,8 +137,8 @@ plot <- function(stats_df, grouping_var, dep_var){
               ylab <- "Count"
               title <- "Cell counts after QC"
             } else {
-              if(deparse(substitute(dep_var)) == "contamination_fraction") {
-                ylab <- "SoupX-inferred\ncontamination fraction"
+              if(deparse(substitute(dep_var)) == "fraction_counts_removed_from_cells") {
+                ylab <- "CellBender-inferred\ncontamination fraction"
                 title <- "Estimated ambient RNA\ncontamination"
               }
             }
@@ -192,9 +221,9 @@ ggsave(paste0(plots_dir, "prefilter_complexity.png"),
        units = "in", dpi = 600,
        height = 6, width = 8)
 
-plot(pre_stats, Tissue, contamination_fraction) + 
-  plot(pre_stats, Group, contamination_fraction) +
-  plot(pre_stats, Batch, contamination_fraction) + 
+plot(pre_stats, Tissue, fraction_counts_removed_from_cells) + 
+  plot(pre_stats, Group, fraction_counts_removed_from_cells) +
+  plot(pre_stats, Batch, fraction_counts_removed_from_cells) + 
   plot_layout(design = design)
 ggsave(paste0(plots_dir, "prefilter_contamination.png"),
        units = "in", dpi = 600,
@@ -204,7 +233,7 @@ ggsave(paste0(plots_dir, "prefilter_contamination.png"),
 
 post_stats_list <- list()
 
-post_stats_files <- list.files(paste0(proj_dir, "tab_data/03_qc2/"),
+post_stats_files <- list.files("tab_data/03_qc2/",
                                full.names = T)
 post_stats_files <- post_stats_files[str_detect(post_stats_files, "median")]
 
@@ -218,7 +247,6 @@ post_stats <- list_rbind(post_stats_list)
 
 post_stats <- post_stats %>%
   dplyr::rename("ID" = "id") %>%
-  mutate(ID = str_replace_all(ID, "GWF-", "GWF")) %>%
   left_join(meta, by = c("ID", "Tissue")) %>%
   mutate(Batch = factor(Batch,
                         levels = c(1:6)),
@@ -229,12 +257,11 @@ post_stats <- post_stats %>%
                          levels = c("Motor cortex", "Cervical spinal cord", "Muscle"),
                          labels = c("Motor cortex", "Cervical\nspinal cord", "Skeletal muscle")))
 
-counts <- read.csv(paste0(proj_dir, "tab_data/03_qc2/filtered_counts.csv")) %>%
+counts <- read.csv("tab_data/03_qc2/filtered_counts.csv") %>%
   dplyr::rename("ID" = "id", "Tissue" = "tissue") %>%
   mutate(Tissue = factor(Tissue,
                          levels = c("Motor cortex", "Cervical spinal cord", "Skeletal muscle"),
-                         labels = c("Motor cortex", "Cervical\nspinal cord", "Skeletal muscle")),
-         ID = str_replace_all(ID, "GWF-", "GWF"))
+                         labels = c("Motor cortex", "Cervical\nspinal cord", "Skeletal muscle")))
 
 post_stats <- post_stats %>%
   left_join(counts, by = c("ID", "Tissue")) %>%
